1dcf2e7a33623716e51cd80bc4554fe2f1df2b05
Vitor Wdson <vitor.wdson2@gmail.com>
2024-01-21 20:37:10 -0400

    Syntax highlighting of SQL queries inside go strings

---
+++ queries/go/injections.scm
@@ -0,0 +1,7 @@
+(call_expression
+  (selector_expression
+    operand: (_) @_operand (#match? @_operand "([dD][bB]([cC][oO][nN][nN]([eE][cC][tT][iI][oO][nN])?)?)|([sS][qQ][lL])")
+    field: (field_identifier) @_field (#any-of? @_field "Exec" "Query" "QueryRow" "QueryContext" "QueryRowContext"))
+  (argument_list
+    (raw_string_literal) @sql (#offset! @sql 0 1 0 -1))
+)
