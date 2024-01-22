5535a460260f9977667451906509087fe45a3ee5
Vitor Wdson <vitor.wdson2@gmail.com>
2024-01-22 14:52:49 -0400

    Enabling winbar via lualine

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -1,3 +1,23 @@
+local lualine_filetype = {
+  'filename',
+  file_status = true,         -- Displays file status (readonly status, modified status)
+  newfile_status = true,     -- Display new file status (new file means no write after created)
+  path = 1,                   -- 0: Just the filename
+  -- 1: Relative path
+  -- 2: Absolute path
+  -- 3: Absolute path, with tilde as the home directory
+  -- 4: Filename and parent dir, with tilde as the home directory
+
+  shorting_target = 40,     -- Shortens path to leave 40 spaces in the window
+  -- for other components. (terrible name, any suggestions?)
+  symbols = {
+    modified = '',          -- Text to show when the file is modified.
+    readonly = '',          -- Text to show when the file is non-modifiable or readonly.
+    unnamed = '[No Name]',     -- Text to show for unnamed buffers.
+    newfile = '',         -- Text to show for newly created file before first write
+  }
+}
+
 return {
   {
     -- Theme inspired by Atom
@@ -27,6 +47,23 @@ return {
         theme = 'dracula',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
+        global_status = true,
+      },
+      winbar = {
+        lualine_y = {
+          'diagnostics',
+        },
+        lualine_z = {
+          lualine_filetype,
+        },
+      },
+      inactive_winbar = {
+        lualine_z = {
+          lualine_filetype,
+        },
+      },
+      sections = {
+        lualine_c = {},
       },
     },
   },
