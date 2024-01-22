3e3a1e1ccc3978ca570328c73568c2edefd59123
Vitor Wdson <vitor.wdson2@gmail.com>
2024-01-21 20:37:01 -0400

    Added treesitter playground

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
+      'nvim-treesitter/playground',
     },
     build = ':TSUpdate',
     config = function()
@@ -101,6 +102,24 @@ return {
             },
           },
         },
+        playground = {
+          enable = true,
+          disable = {},
+          updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
+          persist_queries = false, -- Whether the query persists across vim sessions
+          keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
+          },
+        }
       }
     end
   },
