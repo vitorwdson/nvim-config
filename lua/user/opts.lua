233a68cb2f140dd11bd4a439615494f1e1ca3347
Vitor Wdson <vitor.wdson2@gmail.com>
2024-01-22 14:55:30 -0400

    Fixing splits

--- lua/user/opts.lua
+++ lua/user/opts.lua
@@ -20,10 +20,12 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
+
+vim.opt.splitbelow = false
+vim.opt.splitkeep = "screen"
+vim.opt.splitright = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
