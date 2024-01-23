d03cbda106684623153ee5c661a650100d6acaf8
Vitor Wdson <vitor.wdson2@gmail.com>
2024-01-23 11:44:02 -0400

    Disabled hardtime on fugitive and fixes imcompabilities

--- lua/user/plugins/hardtime.lua
+++ lua/user/plugins/hardtime.lua
@@ -1,5 +1,25 @@
 return {
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {}
+  priority = 1000,
+  opts = {
+    restricted_keys = {
+      ["<CR>"] = {},
+      ["<C-M>"] = {},
+      ["<C-N>"] = {},
+      ["<C-P>"] = {},
+    },
+    disabled_filetypes = {
+      "TelescopePrompt",
+      "checkhealth",
+      "help",
+      "lazy",
+      "mason",
+      "netrw",
+      "noice",
+      "notify",
+      "undotree",
+      "fugitive",
+    }
+  }
 }
