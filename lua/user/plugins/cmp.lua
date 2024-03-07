2cb5258b2f9f38d24f3bc0a12eb8f747c6bd825a
Vitor Wdson <vitor.wdson2@gmail.com>
2024-03-06 23:09:34 -0400

    fix: nvim-cmp next/prev keymaps

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -28,8 +28,8 @@ return {
         },
         completion = { completeopt = 'menu,menuone,noinsert' },
         mapping = cmp.mapping.preset.insert {
-          ['<C-n>'] = cmp.select_next_item,
-          ['<C-p>'] = cmp.select_prev_item,
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete {},
