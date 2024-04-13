77dd8c5eb8340136c2f3daee5821e43850d15d08
Vitor Wdson <vitor.wdson2@gmail.com>
2024-04-13 15:15:31 -0400

    fix: luarocks.nvim setup

--- lua/user/plugins/rest.lua
+++ lua/user/plugins/rest.lua
@@ -1,9 +1,8 @@
 return {
   {
     "vhyrro/luarocks.nvim",
-    config = function()
-      require("luarocks").setup({})
-    end,
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    config = true,
   },
   {
     "rest-nvim/rest.nvim",
