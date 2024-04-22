ce86bd13eccf66c6113fc9ff137dea246f0bd289
Vitor Wdson <vitor.wdson2@gmail.com>
2024-04-22 08:28:25 -0400

    feat: update harpoon config

--- lua/user/plugins/harpoon.lua
+++ lua/user/plugins/harpoon.lua
@@ -15,7 +15,7 @@ return {
         },
       })
 
-      vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
       vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
       vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
