4a600ae35a821b31f75f235aaa227bb8f69a51a3
Vitor Wdson <vitor.wdson2@gmail.com>
2024-05-19 13:26:23 -0400

    feat: keymaps for controlling window splits

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -37,3 +37,29 @@ vim.keymap.set("n", "<leader>r", ":source ~/.config/nvim/init.lua<CR>")
 vim.keymap.set("n", "<leader><leader>x", ":source %<CR>")
 
 vim.keymap.set("i", "<C-u>", "<Nop>")
+
+-- Moving between splits
+vim.keymap.set("n", "<A-h>", "<C-w>h")
+vim.keymap.set("n", "<A-j>", "<C-w>j")
+vim.keymap.set("n", "<A-k>", "<C-w>k")
+vim.keymap.set("n", "<A-l>", "<C-w>l")
+
+-- Resizing splits
+vim.keymap.set("n", "<A-=>", "<C-w>=")
+vim.keymap.set("n", "<A-o>", "<C-w>o")
+vim.keymap.set("n", "<A-s-=>", "<C-w>+")
+vim.keymap.set("n", "<A-x>", "<C-w>5+")
+vim.keymap.set("n", "<A-c>", "<C-w>5-")
+vim.keymap.set("n", "<A-.>", "<C-w>5>")
+vim.keymap.set("n", "<A-,>", "<C-w>5<")
+
+-- Creating splits
+vim.keymap.set("n", "<A-v>", "<C-w>v")
+vim.keymap.set("n", "<A-s>", "<C-w>s")
+
+-- Reordering splits
+vim.keymap.set("n", "<A-r>", "<C-w>r")
+vim.keymap.set("n", "<A-s-h>", "<C-w>H")
+vim.keymap.set("n", "<A-s-j>", "<C-w>J")
+vim.keymap.set("n", "<A-s-k>", "<C-w>K")
+vim.keymap.set("n", "<A-s-l>", "<C-w>L")
