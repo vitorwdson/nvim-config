587f657fe2aa1574d52ea12653611473d2a6d30b
Vitor Wdson <vitor.wdson2@gmail.com>
2024-08-26 20:46:20 -0400

    feat: add sql injection for dbPool names

--- queries/go/injections.scm
+++ queries/go/injections.scm
@@ -1,6 +1,6 @@
 (call_expression
   (selector_expression
-    operand: (_) @_operand (#match? @_operand "([dD][bB]([cC][oO][nN][nN]([eE][cC][tT][iI][oO][nN])?)?)|([sS][qQ][lL])")
+    operand: (_) @_operand (#match? @_operand "([dD][bB]([pP][oO][oO][lL])?([cC][oO][nN][nN]([eE][cC][tT][iI][oO][nN])?)?)|([sS][qQ][lL])")
     field: (field_identifier) @_field (#any-of? @_field "Exec" "Query" "QueryRow" "QueryContext" "QueryRowContext"))
   (argument_list
     (raw_string_literal) @sql (#offset! @sql 0 1 0 -1))
