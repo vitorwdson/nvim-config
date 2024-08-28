49429649a2314c3ddc2f5275dd9962ea72b77653
Vitor Wdson <vitor.wdson2@gmail.com>
2024-08-28 10:19:19 -0400

    feat: django templates formatting/linting

--- lua/user/plugins/null_ls.lua
+++ lua/user/plugins/null_ls.lua
@@ -1,3 +1,5 @@
+local mason_bin_folder = vim.fn.stdpath('data') .. "/mason/bin"
+
 return {
   {
     "nvimtools/none-ls.nvim",
@@ -16,6 +18,7 @@ return {
         "goimports",
         "golines",
         "typescript-language-server",
+        "djlint",
       }
 
       require("mason").setup()
@@ -37,6 +40,7 @@ return {
       end
 
       local null_ls = require("null-ls")
+      local h = require("null-ls.helpers")
 
       null_ls.setup({
         sources = {
@@ -46,7 +50,7 @@ return {
           null_ls.builtins.formatting.prettier,
           null_ls.builtins.formatting.sql_formatter.with({
             generator_opts = {
-              command = "sql-formatter",
+              command = mason_bin_folder .. "/sql-formatter",
               args = { "-c", vim.fn.stdpath("config") .. "/sql-formatter.json" },
               to_stdin = true,
             },
@@ -54,6 +58,46 @@ return {
           null_ls.builtins.formatting.goimports,
           null_ls.builtins.formatting.golines,
           null_ls.builtins.formatting.gofumpt,
+          null_ls.builtins.formatting.djlint.with({
+            generator_opts = {
+              command = mason_bin_folder .. "/djlint",
+              args = {
+                "--configuration",
+                vim.fn.stdpath("config") .. "/.djlintrc",
+                "--reformat",
+                "-",
+              },
+              to_stdin = true,
+            },
+          }),
+          null_ls.builtins.diagnostics.djlint.with({
+            generator_opts = {
+              command = mason_bin_folder .. "/djlint",
+              args = {
+                "--configuration",
+                vim.fn.stdpath("config") .. "/.djlintrc",
+                "--quiet",
+                "-",
+              },
+              to_stdin = true,
+              from_stderr = false,
+              ignore_stderr = true,
+              format = "line",
+              check_exit_code = function(code)
+                return code <= 1
+              end,
+              on_output = h.diagnostics.from_patterns({
+                {
+                  pattern = [[(%w+) (%d+):(%d+) (.*).]],
+                  groups = { "code", "row", "col", "message" },
+                  overrides = {
+                    diagnostic = { severity = INFO },
+                    offsets = { col = 1 },
+                  },
+                },
+              }),
+            },
+          }),
         },
       })
     end
