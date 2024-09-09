3ff427935acc70b8a01b5aaf42a0041dbcd26399
Vitor Wdson <vitor.wdson2@gmail.com>
2024-09-09 15:20:31 -0400

    feat: multiple treesitter context lines

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -128,7 +128,7 @@ return {
 
       require 'treesitter-context'.setup {
         enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
-        max_lines = 1,            -- How many lines the window should span. Values <= 0 mean no limit.
+        max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
         min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
         line_numbers = true,
         multiline_threshold = 20, -- Maximum number of lines to show for a single context
@@ -140,7 +140,7 @@ return {
         zindex = 20,     -- The Z-index of the context window
         on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
       }
-      vim.api.nvim_set_hl(0, "TreesitterContext", { bg = nil, underline = true })
+      -- vim.api.nvim_set_hl(0, "TreesitterContext", { bg = nil, underline = true })
 
       vim.keymap.set("n", "<leader>tsp", ":TSPlaygroundToggle<CR>")
     end
