d704dbacd09c319bdf40f5e10ecfaebe5bd33ada
Vitor Wdson <vitor.wdson2@gmail.com>
2024-11-12 11:05:06 -0400

    feat: add rainbow-delimiters and dracula colors

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -3,6 +3,7 @@ return {
 
   {
     "lukas-reineke/indent-blankline.nvim",
+    dependencies = { "hiphish/rainbow-delimiters.nvim" },
     main = "ibl",
     opts = {},
     config = function()
@@ -14,22 +15,21 @@ return {
         "RainbowOrange",
         "RainbowGreen",
         "RainbowViolet",
-        "RainbowCyan",
       }
 
       local hooks = require "ibl.hooks"
       -- create the highlight groups in the highlight setup hook, so they are reset
       -- every time the colorscheme changes
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#ff5555" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#f1fa8c" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#8be9fd" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#ffb86c" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#50fa7b" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#bd93f9" })
       end)
 
+      vim.g.rainbow_delimiters = { highlight = highlight }
       require("ibl").setup { indent = { highlight = highlight, char = "┊" } }
     end
   },
