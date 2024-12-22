ef1cdd5426b68722cb2675d592f0c7d33fc500b3
Vitor Wdson <vitor.wdson2@gmail.com>
2024-12-22 01:27:05 -0400

    feat: revert change

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -134,7 +134,8 @@ return {
       local mason_lspconfig = require 'mason-lspconfig'
 
       mason_lspconfig.setup {
-        -- ensure_installed = vim.tbl_keys(servers),
+        ensure_installed = vim.tbl_keys(servers),
+        automatic_installation = true,
       }
       mason_lspconfig.setup_handlers {
         function(server_name)
