2d603a3238d4d02ddcc9a099106822577de8a85f
Vitor Wdson <vitor.wdson2@gmail.com>
2025-08-05 15:25:09 -0400

    fix: basedpyright configuration

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -1,14 +1,14 @@
 return {
-  pyright = {
-    -- Using Ruff's import organizer
-    disableOrganizeImports = true,
-  },
-  python = {
-    analysis = {
-      autoSearchPaths = true,
-      useLibraryCodeForTypes = true,
-      diagnosticMode = 'openFilesOnly',
-      -- ignore = { '*' },
+  settings = {
+    basedpyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "openFilesOnly",
+        typeCheckingMode = "standard", -- TODO: switch to recommended and override on the projects config file
+      },
     },
   },
 }
