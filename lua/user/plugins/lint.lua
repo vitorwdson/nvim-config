e1a6962dc0072461c00c209de4bd658f299752c6
Vitor Wdson <vitor.wdson2@gmail.com>
2025-11-05 11:00:40 -0400

    feat: add bandit linter

--- lua/user/plugins/lint.lua
+++ lua/user/plugins/lint.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     require('lint').linters_by_ft = {
       htmldjango = {'djlint'},
+      python = {'bandit'},
     }
 
     local djlint = require("lint").linters.djlint
--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -148,6 +148,7 @@ return {
         "stylua",
         "biome",
         "templ",
+        "bandit",
       },
     },
     enabled = function()
